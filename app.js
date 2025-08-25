9b1e413a76f22e9f435436a83eec1ede47c7421f
Alfred <fna.dev.app.com>
2025-08-26 00:48:52 +0200

    fdh

--- app.js
+++ app.js
@@ -99,15 +99,21 @@ app.post("/docx-to-pdf", upload.single("file"), (req, res) => {
 
 
 // PDF → DOCX
-const uploadd = multer({ storage: multer.memoryStorage(), limits: { fileSize: 50 * 1024 * 1024 } });
-app.post("/pdf-to-docx", uploadd.single("file"), (req, res) => {
-  const pdfBuffer = req.file.buffer;
+//const uploadd = multer({ storage: multer.memoryStorage(), limits: { fileSize: 50 * 1024 * 1024 } });
+app.post("/pdf-to-docx", upload.single("file"), (req, res) => {
+  const filePath = req.file.path;
+
+  const pdfBuffer = fs.readFileSync(filePath);
   process.env.TMPDIR = "/tmp"; // oder ein eigener Ordner
   // PDF → DOCX konvertieren
   libre.convert(pdfBuffer, ".docx", undefined, (err, done) => {
+    fs.unlink(filePath, unlinkErr => {
+      if (unlinkErr) console.error("Fehler beim Löschen der Datei:", unlinkErr);
+    });
+
     if (err) {
       console.error("Konvertierungsfehler:", err);
-      return res.status(500).send(err);
+      return res.status(500).send("Konvertierung fehlgeschlagen");
     }
 
     // Download erzwingen
